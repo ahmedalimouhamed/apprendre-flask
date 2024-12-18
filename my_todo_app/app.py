e9d7ced276230c6fd69751024c1d93817a8f9d77
zerzif-chouaib-git <zerzif.bouchaib@gmail.com>
2024-12-18 21:19:54 +0100

    1st commit

---
+++ my_todo_app/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///asks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form.get('task')
+    if task_content:
+        new_task = Task(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
